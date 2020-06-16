27b987178e0ca38246a7dd85c68ce5b53e708539
myskamil <kamil.myslek@silevis.com>
2020-06-16 12:56:08 +0200

    add possibility to scroll on sticky when is needed

--- src/lib/Functions/componentDidUpdate.ts
+++ src/lib/Functions/componentDidUpdate.ts
@@ -13,8 +13,6 @@ export function componentDidUpdate(prevProps: ReactGridProps, prevState: State, 
     const location = state.focusedLocation;
     if (location) {
         const shouldChangeScroll = !areLocationsEqual(location, prevState.focusedLocation)
-            && !isFocusLocationOnLeftSticky(state, location)
-            && !isFocusLocationOnTopSticky(state, location);
         if (shouldChangeScroll) {
             const top = getScrollTop(state, location);
             const left = getScrollLeft(state, location);
@@ -30,7 +28,9 @@ function getScrollTop(state: State, location: Location): number {
     const visibleScrollAreaHeight = getVisibleScrollAreaHeight(state, wholeStickyHeight);
     const { top } = getReactGridOffsets(state);
     const topStickyOffset = getStickyOffset(scrollTop, top);
-
+    if (isFocusLocationOnTopSticky(state, location)) {
+        return scrollTop;
+    }
     if (isBottomCellAllVisible(state, location, visibleScrollAreaHeight)) {
         return getCalculatedScrollTopValueToBottom(location, visibleScrollAreaHeight, scrollTop, topStickyOffset);
     } else if (isTopCellAllVisible(state, location)) {
@@ -46,7 +46,9 @@ function getScrollLeft(state: State, location: Location): number {
     const visibleScrollAreaWidth = getVisibleScrollAreaWidth(state, wholeStickyWidth)
     const { left } = getReactGridOffsets(state);
     const leftStickyOffset = getStickyOffset(scrollLeft, left);
-
+    if (isFocusLocationOnLeftSticky(state, location)) {
+        return scrollLeft;
+    }
     if (isRightCellAllVisible(state, location, visibleScrollAreaWidth)) {
         return getCalculatedScrollLeftValueToRight(location, visibleScrollAreaWidth, scrollLeft, leftStickyOffset);
     } else if (isLeftCellAllVisible(state, location)) {
