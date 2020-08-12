446988da6c98b641bc958c0fc0397a78fa27a251
Patryk Eliasz <patryk0493@gmail.com>
2020-08-12 10:01:18 +0200

    moved out handleStateUpdate fn from ReactGrid component

--- src/lib/Components/ReactGrid.tsx
+++ src/lib/Components/ReactGrid.tsx
@@ -11,9 +11,12 @@ import { LegacyBrowserGridRenderer } from './LegacyBrowserGridRenderer';
 import { CellEditorRenderer, cellEditorCalculator } from './CellEditor';
 import { CellRenderer, } from './CellRenderer';
 import { notifyAboutReactGridPro } from '../Functions/notifyAboutReactGridPro';
+import { handleStateUpdate } from '../Functions/handleStateUpdate';
+
 
 export class ReactGrid extends React.Component<ReactGridProps, State> {
-    private stateUpdater: StateUpdater = modifier => this.handleStateUpdate(modifier(this.state));
+    private updateState = (state: State) => this.setState(state);
+    private stateUpdater: StateUpdater = modifier => handleStateUpdate(modifier(this.state), this.state, this.props, this.updateState);
     private pointerEventsController = new PointerEventsController(this.stateUpdater);
     private eventHandlers: EventHandlers = new EventHandlers(this.stateUpdater, this.pointerEventsController);
     private cellMatrixBuilder = new CellMatrixBuilder();
@@ -56,17 +59,4 @@ export class ReactGrid extends React.Component<ReactGridProps, State> {
         }
     }
 
-    // TODO create function
-    private handleStateUpdate(newState: State) {
-        const changes = [...newState.queuedCellChanges];
-        if (changes.length > 0) {
-            if (this.props.onCellsChanged) {
-                this.props.onCellsChanged([...changes]);
-            };
-            changes.forEach(() => newState.queuedCellChanges.pop());
-        }
-        if (newState !== this.state) {
-            this.setState(newState);
-        }
-    }
 };
