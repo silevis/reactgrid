0e8742644a9f483f3cd95bf724da3cb25b7a05d2
Patryk Eliasz <patryk0493@gmail.com>
2020-11-03 16:11:23 +0100

    custom dropdown draft

--- src/lib/CellTemplates/DropdownCellTemplate.tsx
+++ src/lib/CellTemplates/DropdownCellTemplate.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 // NOTE: all modules imported below may be imported from '@silevis/reactgrid'
 import { getCellProperty } from '../Functions/getCellProperty';
+import { keyCodes } from '../Functions/keyCodes';
 import { Cell, CellTemplate, Compatible, Uncertain, UncertainCompatible } from '../Model/PublicModel';
 
 export interface DropdownCell extends Cell {
@@ -9,6 +10,7 @@ export interface DropdownCell extends Cell {
     key: string;
     values: string[];
     disabled?: boolean;
+    isOpen?: boolean;
 }
 
 export class DropdownCellTemplate implements CellTemplate<DropdownCell> {
@@ -23,32 +25,76 @@ export class DropdownCellTemplate implements CellTemplate<DropdownCell> {
         } catch {
             disabled = false;
         }
-        return { ...uncertainCell, key, text: key, value, values, disabled }
+        let isOpen: boolean;
+        try {
+            isOpen = getCellProperty(uncertainCell, 'isOpen', 'boolean');
+        } catch {
+            isOpen = false;
+        }
+        return { ...uncertainCell, key, text: key, value, values, disabled, isOpen };
     }
 
     update(cell: Compatible<DropdownCell>, cellToMerge: UncertainCompatible<DropdownCell>): Compatible<DropdownCell> {
-        return this.getCompatibleCell({ ...cell, key: cellToMerge.key });
+        return this.getCompatibleCell({ ...cell, key: cellToMerge.key, isOpen: cellToMerge.isOpen });
     }
 
     getClassName(cell: Compatible<DropdownCell>, isInEditMode: boolean) {
         return cell.className ? cell.className : '';
     }
-    //TODO open dropdown cell templete with space
+
+    handleKeyDown(cell: Compatible<DropdownCell>, keyCode: number, ctrl: boolean, shift: boolean, alt: boolean): { cell: Compatible<DropdownCell>, enableEditMode: boolean } {
+        if (keyCode === keyCodes.SPACE && !shift) {
+            return { cell: { ...cell, isOpen: true }, enableEditMode: false };
+        }
+        return { cell, enableEditMode: false };
+    }
+
     render(
         cell: Compatible<DropdownCell>,
         isInEditMode: boolean,
         onCellChanged: (cell: Compatible<DropdownCell>, commit: boolean) => void
     ): React.ReactNode {
-        return (
-            <select
-                disabled={cell.disabled}
-                className={cell.className}
-                onPointerDown={e => e.stopPropagation()}
-                defaultValue={cell.text}
-                onChange={e => onCellChanged(this.getCompatibleCell({ ...cell, key: e.currentTarget.value }), true)}
+        const dropdownRef = React.useRef<any>(null);
+        console.log(cell.isOpen);
+
+        React.useEffect(() => {
+            const pageClickEvent = (e: any) => {
+                if (dropdownRef.current !== null && !dropdownRef.current.contains(e.target)) {
+                    onCellChanged(this.getCompatibleCell({ ...cell, isOpen: !cell.isOpen }), true);
+                }
+            };
+
+            if (cell.isOpen) {
+                window.addEventListener('pointerdown', pageClickEvent);
+            }
+
+            return () => window.removeEventListener('pointerdown', pageClickEvent);
+
+        }, [cell.isOpen, cell, onCellChanged]);
+
+        return (<>
+            <button className="menu-trigger"
+                onPointerDown={e => {
+                    e.stopPropagation();
+                    onCellChanged(this.getCompatibleCell({ ...cell, isOpen: !cell.isOpen }), true);
+                }}
             >
-                {cell.values.map((value, idx) => <option key={idx} value={value}>{value}</option>)}
-            </select>
-        );
+                <span>{cell.key}</span>
+            </button>
+            <div
+                ref={dropdownRef}
+                className={`dropdown-container ${cell.isOpen ? 'open' : 'closed'}`}
+                onPointerDown={e => e.stopPropagation()}
+            >
+                <ul>
+                    {cell.values.map((value, idx) => <li key={idx} onPointerDown={e => {
+                        // setIsActive(false);
+                        onCellChanged(this.getCompatibleCell({ ...cell, isOpen: false, key: cell.values[idx] }), true)
+                    }}>
+                        {value}
+                    </li>)}
+                </ul>
+            </div >
+        </>);
     }
 }
