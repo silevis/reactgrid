34c5a6eb8eea03c267ccde829bfd463a954a0b5b
myskamil <kamil.myslek@silevis.com>
2020-12-04 14:58:45 +0100

    add basic pinned to body sticky tests

--- cypress/integration/tests/stickyPinnedToBody/scroll.spec.ts
+++ cypress/integration/tests/stickyPinnedToBody/scroll.spec.ts
@@ -1,6 +1,10 @@
 /// <reference types="Cypress" />
-
 import { visitStickyPinnedToBody } from '../../common/visit';
+import { constants } from '../../common/constants';
+import { Utilities } from '../../common/utils';
+import { enablePinnedToBodyConfig as config } from '../../../../src/test/testEnvConfig';
+
+const utils = new Utilities(config);
 
 context('Sticky pinned to body - scroll', () => {
 
@@ -8,7 +12,23 @@ context('Sticky pinned to body - scroll', () => {
         visitStickyPinnedToBody();
     });
 
-    it('', () => { // ✅
+    it('should scroll to bottom, enter to scrollable area with keys and scroll to top', () => { // ✅
+        utils.scrollToBottom();
+        utils.assertScrolledToBottom();
+        utils.selectCell(config.cellWidth * 2 + utils.getCellXCenter(), utils.getCellYCenter(), { force: true });
+        for (let i = 0; i < config.stickyTop; i++) {
+            utils.keyDown(constants.keyCodes.ArrowDown, { force: true });
+        };
+        utils.assertScrolledToTop();
+    });
+
+    it('should scroll to right and enter to scrollable area with keys and scroll to left', () => { // ✅
+        utils.scrollToRight();
+        utils.selectCell(utils.getCellXCenter(), config.cellHeight * 5 + utils.getCellYCenter(), { force: true });
+        for (let i = 0; i < config.stickyLeft; i++) {
+            utils.keyDown(constants.keyCodes.ArrowRight, { force: true });
+        };
+        utils.assertScrolledToLeft();
     });
 
 });
