99075197c6967379bf5f32a84d19b1b43d7c4180
Patryk Eliasz <patryk0493@gmail.com>
2020-12-15 12:14:11 +0100

    amended editor test on both axis scrolled view

--- cypress/integration/tests/standard/cellEditor.spec.ts
+++ cypress/integration/tests/standard/cellEditor.spec.ts
@@ -33,25 +33,28 @@ context('Cell editor position', () => {
   });
 
   it('should open fixed cell editor on both axis scrolled view', () => { // ✅ 
-    [{
-      click: {
-        x: config.cellWidth * 1,
-        y: config.cellHeight * 2,
+    [
+      {
+        click: {
+          x: config.cellWidth * 1,
+          y: config.cellHeight * 2,
+        },
+        scroll: {
+          x: config.cellWidth + 10,
+          y: config.cellHeight - 12,
+        },
       },
-      scroll: {
-        x: config.cellWidth,
-        y: config.cellHeight,
-      },
-    }, {
-      click: {
-        x: config.cellWidth * utils.getRandomInt(1, 5),
-        y: config.cellHeight * utils.getRandomInt(1, 15),
-      },
-      scroll: {
-        x: config.cellWidth * 20 + utils.getRandomInt(1, config.cellWidth),
-        y: config.cellHeight * 25 + utils.getRandomInt(1, config.cellHeight),
-      },
-    }].forEach(utils.testCellEditor.bind(utils));
+      {
+        click: {
+          x: config.cellWidth * utils.getRandomInt(1, 5),
+          y: config.cellHeight * utils.getRandomInt(1, 15),
+        },
+        scroll: {
+          x: config.cellWidth * 20 + utils.getRandomInt(1, config.cellWidth),
+          y: config.cellHeight * 25 + utils.getRandomInt(1, config.cellHeight),
+        },
+      }
+    ].forEach(utils.testCellEditor.bind(utils));
   });
 
   it('should open a cell editor in the vertically scrolled view', () => { // ✅ 
