97a3ee6777cf2046bfaadb955e60024fa52d44e8
Patryk Eliasz <patryk0493@gmail.com>
2021-01-05 13:28:39 +0100

    removed newLocation fn

--- src/lib/Functions/newLocation.ts
+++
@@ -1,5 +0,0 @@
-import { GridColumn, GridRow } from '../Model/InternalModel';
-import { Location } from '../Model/InternalModel';
-
-// just a helper, much quicker than newLocation()!
-export const newLocation = (row: GridRow, column: GridColumn): Location => ({ row, column })
