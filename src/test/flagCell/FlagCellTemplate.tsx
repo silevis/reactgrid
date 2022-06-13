7f53d50d155d2b03e37cd676b03612dd604f903a
alonshmiel <alon.shmiel@gmail.com>
2022-06-13 16:31:07 +0300

    Esc cancels cell value changes (#97)
    fix esc cancels cell value changes

--- src/test/flagCell/FlagCellTemplate.tsx
+++ src/test/flagCell/FlagCellTemplate.tsx
@@ -48,7 +48,7 @@ export class FlagCellTemplate implements CellTemplate<FlagCell> {
             onCut={e => e.stopPropagation()}
             onPaste={e => e.stopPropagation()}
             onPointerDown={e => e.stopPropagation()}
-            onBlur={e => onCellChanged(this.getCompatibleCell({ ...cell, text: e.currentTarget.value }), true)}
+            onBlur={e => onCellChanged(this.getCompatibleCell({ ...cell, text: e.currentTarget.value }), (e as any).view?.event?.keyCode !== keyCodes.ESCAPE)}
             onKeyDown={e => {
                 if (isAlphaNumericKey(e.keyCode) || isNavigationKey(e.keyCode)) e.stopPropagation();
                 if (e.keyCode === keyCodes.ESCAPE) e.currentTarget.value = cell.text; // reset
