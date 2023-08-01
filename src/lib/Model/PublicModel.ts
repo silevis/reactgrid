64447431bca3250f833a3302b24e39b2d5e97231
Daniel Fidor <65829376+DLowHP@users.noreply.github.com>
2023-08-01 14:01:59 +0200

    added a disclaimer

--- src/lib/Model/PublicModel.ts
+++ src/lib/Model/PublicModel.ts
@@ -331,7 +331,7 @@ export interface CellTemplate<TCell extends Cell = Cell> {
      * @param {boolean} ctrl Is `ctrl` pressed when event is called ()
      * @param {boolean} shift Is `shift` pressed when event is called
      * @param {boolean} alt Is `alt` pressed when event is called
-     * @param {string} key Represents the value of the key pressed by the user
+     * @param {string} [key] Represents the value of the key pressed by the user. Optional for backwards compatibility.
      * @returns {{ cell: Compatible<TCell>; enableEditMode: boolean }} Cell data and edit mode either affected by the event or not
     */
     handleKeyDown?(
