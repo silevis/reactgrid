304ccc9eb866559438f4801c1f5567f41dbcea76
Daniel Fidor <daniel.fidor@silevis.com>
2023-11-07 10:30:36 +0100

    ladle story update

--- src/stories/BigGrid.stories.tsx
+++ src/stories/BigGrid.stories.tsx
@@ -1,71 +1,89 @@
 import { StoryDefault } from "@ladle/react";
-import { useEffect } from "react";
+import { StrictMode, useState } from "react";
 import ReactGrid from "../ReactGrid";
 import { ErrorBoundary } from "../components/ErrorBoundary";
+import TextCell from "../components/cellTemplates/TextCell";
 import { cellMatrixBuilder } from "../utils/cellMatrixBuilder";
 
-type CellSize = {
-  width: number;
-  height: number;
-};
+const ROW_COUNT = 50;
+const COLUMN_COUNT = 40;
 
 export const BigGrid = () => {
-  const cellMatrix = cellMatrixBuilder(
-    ({ addRows, addColumns, setCell }) => {
-      Array.from({ length: 1000 }).forEach((_, i) => {
-        addRows({
-          id: i.toString(),
-          height: `${Math.floor(Math.random() * 5) + 1}fr`,
-        });
-      });
-      Array.from({ length: 200 }).forEach((_, i) => {
-        addColumns({
-          id: i.toString(),
-          width: `${Math.floor(Math.random() * 5) + 1}fr`,
-        });
-      });
+  const [data, setData] = useState<(string | null)[][]>(
+    Array.from({ length: ROW_COUNT }).map((_, i) => {
+      return Array.from({ length: COLUMN_COUNT }).map((_, j) => {
+        if (i === 0 && j === 1) return null;
+        if (i === 1 && j === 0) return null;
+        if (i === 1 && j === 1) return null;
 
-      Array.from({ length: 1000 }).forEach((_, i) => {
-        Array.from({ length: 200 }).forEach((_, j) => {
-          setCell(i.toString(), j.toString(), () => (
-            <div
-              style={{ padding: ".1rem .2rem", textAlign: "center" }}
-            >{`[${i}×${j}]`}</div>
-          ));
-        });
+        return [
+          "Lorem ipsum dolor sit amet",
+          "Reiciendis illum, nihil, ab officiis explicabo!",
+          "Excepturi in adipisci omnis illo eveniet obcaecati!",
+          "Doloremque, sit!",
+        ][Math.floor(Math.random() * 4)];
       });
-    }
+    })
   );
 
-  // const [renderingPhase, setRenderingPhase] = React.useState<"initial" | "measuring" | "render">("initial");
-  // const cellSizes = useRef();
-  // const scr
+  const cellMatrix = cellMatrixBuilder(({ addRows, addColumns, setCell }) => {
+    data.forEach((row, i) => {
+      addRows({
+        id: i.toString(),
+        // height: `${Math.floor(Math.random() * 5) + 1}fr`,
+        height: "max-content",
+      });
+      row.forEach((val, j) => {
+        if (i === 0)
+          addColumns({
+            id: j.toString(),
+            // width: `${Math.floor(Math.random() * 3) + 1}fr`,
+            width: "min-content",
+          });
+        if (val === null) return;
 
-  // const getVisibleRange =
+        setCell(i.toString(), j.toString(), TextCell, {
+          text: val,
+          onTextChanged: (newText) => {
+            setData((prev) => {
+              const next = [...prev];
+              next[i][j] = newText;
+              return next;
+            });
+          },
+        });
+      });
+    });
 
-  useEffect(() => {
-    // window.addEventListener("resize", () => {
-  }, []);
+    setCell("0", "0", TextCell, { text: data[0][0] ?? "", reverse: true }, { colSpan: 2, rowSpan: 2 });
+  });
+
+  cellMatrix.columns[1].width = "7rem";
 
   return (
-    <div
-      className="rgScrollableContainer"
-      style={{ height: "300px", width: "450px", overflow: "auto" }}
-    >
-      <ReactGrid
-        id="big-grid"
-        {...cellMatrix}
-      />
-    </div>
+    <>
+      <div className="rgScrollableContainer" style={{ height: "100%", width: "100%", overflow: "auto" }}>
+        <ReactGrid
+          id="big-grid"
+          stickyTopRows={2}
+          stickyLeftColumns={3}
+          stickyRightColumns={2}
+          stickyBottomRows={2}
+          {...cellMatrix}
+        />
+      </div>
+    </>
   );
 };
 
 export default {
   decorators: [
     (Component) => (
-      <ErrorBoundary>
-        <Component />
-      </ErrorBoundary>
+      <StrictMode>
+        <ErrorBoundary>
+          <Component />
+        </ErrorBoundary>
+      </StrictMode>
     ),
   ],
 } satisfies StoryDefault;
