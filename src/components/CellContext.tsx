2fbe0c4c84f268894de34ae49caeabaa9fd525c3
Daniel Fidor <daniel.fidor@silevis.com>
2023-12-13 17:01:31 +0100

    cell wrapper and context update move div inside wrapper, simplify context, update previous usages accordingly

---
+++ src/components/CellContext.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, useContext } from "react";
+import { CellContextType } from "../types/PublicModel";
+
+export const CellContext = createContext<CellContextType>({
+  rowId: "",
+  colId: "",
+  realRowIndex: -1,
+  realColumnIndex: -1,
+  requestFocus: function (_enableEditMode: boolean): void {
+    throw new Error("Function not implemented.");
+  },
+  disableEditMode: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  isInEditMode: false,
+  isFocused: false,
+  containerStyle: {},
+});
+
+export const useCellContext = () => {
+  const ctx = useContext(CellContext);
+
+  if (!ctx) {
+    throw new Error("CellContext is unavailable! Did you use this hook outside of ReactGrid's PaneGridRenderer?");
+  }
+  
+  return ctx;
+}
