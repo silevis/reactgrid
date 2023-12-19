0c312a35a1338c64c170c4ee17df4e918ce11462
Michota <dawidmichota@gmail.com>
2023-12-19 11:08:06 +0100

    chore: comment-out and format code

--- src/behaviors/CellSelectionBehavior.ts
+++ src/behaviors/CellSelectionBehavior.ts
@@ -1,103 +1,110 @@
 import { Behavior, BehaviorConstructor } from "../types/Behavior";
 import { Cell } from "../types/PublicModel";
-import { findMinimalSelectedArea, isCellInRange } from "../utils/cellUtils";
+import {
+  findMinimalSelectedArea,
+  getCellPane,
+  getStickyDirection,
+  isCellInRange,
+  isCellSticky,
+} from "../utils/cellUtils";
 import { getLocationFromClient } from "../utils/getLocationFromClient";
+import { getCellFromPointer } from "../utils/getCellFromPointer";
 import { ReactGridStore } from "../utils/reactGridStore";
 import { getScrollOfScrollableElement, getScrollableParent, getSizeOfScrollableElement } from "../utils/scrollHelpers";
 
-const tryExpandingTowardsPointer = (store: ReactGridStore, x: number, y: number) => {
-  if (!store.reactGridRef) return store;
+// const tryExpandingTowardsPointer = (store: ReactGridStore, x: number, y: number) => {
+//   if (!store.reactGridRef) return store;
 
-  const selectedArea = store.selectedArea;
-  // const focusIndicatorEl = document.getElementsByClassName("rgFocusIndicator")[0] as HTMLElement;
-  const selectionIndicatorEl = document.getElementsByClassName("rgSelectionIndicator")[0] as HTMLElement;
-  
-  if (!selectionIndicatorEl) return store;
+//   const selectedArea = store.selectedArea;
+//   // const focusIndicatorEl = document.getElementsByClassName("rgFocusIndicator")[0] as HTMLElement;
+//   const selectionIndicatorEl = document.getElementsByClassName("rgSelectionIndicator")[0] as HTMLElement;
 
-  const { top, right, bottom, left } = selectionIndicatorEl.getBoundingClientRect();
+//   if (!selectionIndicatorEl) return store;
 
-  let colIndex = -1;
-  let rowIndex = -1;
+//   const { top, right, bottom, left } = selectionIndicatorEl.getBoundingClientRect();
 
-  const getXWithinSelection = (x: number) => {
-    if (x < left) return left + 1;
-    if (x > right) return right - 1;
-    return right - left;
-  }
+//   let colIndex = -1;
+//   let rowIndex = -1;
 
-  window.currentTarget = { rowIndex: -1, colIndex: -1, x, y };
+//   const getXWithinSelection = (x: number) => {
+//     if (x < left) return left + 1;
+//     if (x > right) return right - 1;
+//     return right - left;
+//   };
 
-  if (y < top) rowIndex = selectedArea.startRowIdx - 1;
-  else if (y > bottom) rowIndex = selectedArea.endRowIdx + 1;
-  else rowIndex = getLocationFromClient(store, getXWithinSelection(x), y).rowIndex;
-  window.currentTarget.rowIndex = rowIndex;
-  window.currentTarget.x = getXWithinSelection(x);
+//   if (y < top) rowIndex = selectedArea.startRowIdx - 1;
+//   else if (y > bottom) rowIndex = selectedArea.endRowIdx + 1;
+//   else rowIndex = getLocationFromClient(store, getXWithinSelection(x), y).rowIndex;
 
-  const getYWithinSelection = (y: number) => {
-    if (y < top) return top + 1;
-    if (y > bottom) return bottom - 1;
-    return y;
-  }
+//   const getYWithinSelection = (y: number) => {
+//     if (y < top) return top + 1;
+//     if (y > bottom) return bottom - 1;
+//     return y;
+//   };
 
-  if (x < left) colIndex = selectedArea.startColIdx - 1;
-  else if (x > right) colIndex = selectedArea.endColIdx + 1;
-  else colIndex = getLocationFromClient(store, x, getYWithinSelection(y)).colIndex;
-  window.currentTarget.colIndex = colIndex;
+//   if (x < left) colIndex = selectedArea.startColIdx - 1;
+//   else if (x > right) colIndex = selectedArea.endColIdx + 1;
+//   else colIndex = getLocationFromClient(store, x, getYWithinSelection(y)).colIndex;
 
-  const cell = store.getCellByIndexes(rowIndex, colIndex);
-  if (!cell) return store; 
+//   const cell = store.getCellByIndexes(rowIndex, colIndex);
+//   if (!cell) return store;
 
-  return tryExpandingTowardsCell(store, cell, rowIndex, colIndex);
+//   return tryExpandingTowardsCell(store, cell, rowIndex, colIndex);
 
-  // if (y < top) {
-  //   // Pointer is above the selection indicator on the row axis
-  //   // colIndex = getLocationFromClient(store, x, top + 1).colIndex;
-  //   const { colIndex } = getLocationFromClient(store, x, top + 1);
-  //   const cell = store.getCellByIndexes(selectedArea.startRowIdx - 1, colIndex);
+//   // if (y < top) {
+//   //   // Pointer is above the selection indicator on the row axis
+//   //   // colIndex = getLocationFromClient(store, x, top + 1).colIndex;
+//   //   const { colIndex } = getLocationFromClient(store, x, top + 1);
+//   //   const cell = store.getCellByIndexes(selectedArea.startRowIdx - 1, colIndex);
 
-  //   if (!cell) return store;
+//   //   if (!cell) return store;
 
-  //   return tryExpandingTowardsCell(store, cell, selectedArea.startRowIdx - 1, colIndex);
-  // } else if (y > bottom) {
-  //   // Pointer is below the selection indicator on the row axis
-  //   // colIndex = getLocationFromClient(store, x, bottom - 1).colIndex;
-  //   const { colIndex } = getLocationFromClient(store, x, bottom - 1);
-  //   const cell = store.getCellByIndexes(selectedArea.endRowIdx - 1, colIndex);
+//   //   return tryExpandingTowardsCell(store, cell, selectedArea.startRowIdx - 1, colIndex);
+//   // } else if (y > bottom) {
+//   //   // Pointer is below the selection indicator on the row axis
+//   //   // colIndex = getLocationFromClient(store, x, bottom - 1).colIndex;
+//   //   const { colIndex } = getLocationFromClient(store, x, bottom - 1);
+//   //   const cell = store.getCellByIndexes(selectedArea.endRowIdx - 1, colIndex);
 
-  //   if (!cell) return store;
+//   //   if (!cell) return store;
 
-  //   return tryExpandingTowardsCell(store, cell, selectedArea.endRowIdx, colIndex);
-  // }
+//   //   return tryExpandingTowardsCell(store, cell, selectedArea.endRowIdx, colIndex);
+//   // }
 
-  // if (x < left) {
-  //   // Pointer is before the selection indicator on the column axis
-  //   // rowIndex = getLocationFromClient(store, left + 1, y).rowIndex;
-  //   const { rowIndex } = getLocationFromClient(store, left + 1, y);
-  //   const cell = store.getCellByIndexes(rowIndex, selectedArea.startColIdx - 1);
+//   // if (x < left) {
+//   //   // Pointer is before the selection indicator on the column axis
+//   //   // rowIndex = getLocationFromClient(store, left + 1, y).rowIndex;
+//   //   const { rowIndex } = getLocationFromClient(store, left + 1, y);
+//   //   const cell = store.getCellByIndexes(rowIndex, selectedArea.startColIdx - 1);
 
-  //   if (!cell) return store;
+//   //   if (!cell) return store;
 
-  //   return tryExpandingTowardsCell(store, cell, rowIndex, selectedArea.startColIdx - 1);
-  // } else if (x > right) {
-  //   // Pointer is after the selection indicator on the column axis
-  //   // rowIndex = getLocationFromClient(store, right - 1, y).rowIndex;
-  //   const { rowIndex } = getLocationFromClient(store, right - 1, y);
-  //   const cell = store.getCellByIndexes(rowIndex, selectedArea.endColIdx - 1);
+//   //   return tryExpandingTowardsCell(store, cell, rowIndex, selectedArea.startColIdx - 1);
+//   // } else if (x > right) {
+//   //   // Pointer is after the selection indicator on the column axis
+//   //   // rowIndex = getLocationFromClient(store, right - 1, y).rowIndex;
+//   //   const { rowIndex } = getLocationFromClient(store, right - 1, y);
+//   //   const cell = store.getCellByIndexes(rowIndex, selectedArea.endColIdx - 1);
 
-  //   if (!cell) return store;
+//   //   if (!cell) return store;
 
-  //   return tryExpandingTowardsCell(store, cell, rowIndex, selectedArea.endColIdx - 1);
-  // }
+//   //   return tryExpandingTowardsCell(store, cell, rowIndex, selectedArea.endColIdx - 1);
+//   // }
 
-  return store;
+//   // return store;
 
-  // const cell = store.getCellByIndexes(rowIndex, colIndex);
-  // if (!cell) return store; 
+//   // const cell = store.getCellByIndexes(rowIndex, colIndex);
+//   // if (!cell) return store;
 
-  // return tryExpandingTowardsCell(store, cell, rowIndex, colIndex);
-}
+//   // return tryExpandingTowardsCell(store, cell, rowIndex, colIndex);
+// };
 
-const tryExpandingTowardsCell = (store: ReactGridStore, cell: Cell, rowIndex: number, colIndex: number): ReactGridStore => {
+const tryExpandingTowardsCell = (
+  store: ReactGridStore,
+  cell: Cell,
+  rowIndex: number,
+  colIndex: number
+): ReactGridStore => {
   const selectedArea = store.selectedArea;
   const focusedLocation = store.focusedLocation;
 
@@ -141,12 +148,18 @@ const tryExpandingTowardsCell = (store: ReactGridStore, cell: Cell, rowIndex: nu
       ...newSelectedArea,
     },
   };
-}
+};
 
+const handleSelectionOnStickyPane = (
+  store: ReactGridStore,
+  cell: Cell,
+  rowIndex: number,
+  colIndex: number,
+  x: number,
+  y: number
+): ReactGridStore => {
+  if (!store.reactGridRef) throw new Error("ReactGridRef is not assigned!");
 
-const handleSelectionOnStickyPane = (store: ReactGridStore, cell: Cell, rowIndex: number, colIndex: number, x: number, y: number): ReactGridStore => {
-  if (!store.reactGridRef) throw new Error('ReactGridRef is not assigned!');
-  
   const selectedArea = store.selectedArea;
   const scrollableElement = getScrollableParent(store.reactGridRef, false);
   const { scrollTop, scrollHeight, scrollLeft, scrollWidth } = getScrollOfScrollableElement(scrollableElement);
@@ -164,7 +177,7 @@ const handleSelectionOnStickyPane = (store: ReactGridStore, cell: Cell, rowIndex
     }
   } else if (isCellInRange(store, cell, store.paneRanges.Right)) {
     scrollableElement?.scrollBy({ left: 10 });
-    if (Math.abs((scrollLeft + width) - scrollWidth) > 5) {
+    if (Math.abs(scrollLeft + width - scrollWidth) > 5) {
       // The grid is not scrolled to the right, but the pointer is on the right pane
       // so we need to find the indexes of the cell on the right of the selection in the row targeted by the pointer
       const cell = store.getCellByIndexes(rowIndex, selectedArea.endColIdx + 1);
@@ -174,7 +187,7 @@ const handleSelectionOnStickyPane = (store: ReactGridStore, cell: Cell, rowIndex
     }
   } else if (isCellInRange(store, cell, store.paneRanges.BottomCenter)) {
     scrollableElement?.scrollBy({ top: 10 });
-    if (Math.abs((scrollTop + height) - scrollHeight) > 5) {
+    if (Math.abs(scrollTop + height - scrollHeight) > 5) {
       // The grid is not scrolled to the bottom, but the pointer is on the bottom pane
       // so we need to find the indexes of the cell below the selection in the column targeted by the pointer
       const cell = store.getCellByIndexes(selectedArea.endRowIdx + 1, colIndex);
@@ -196,7 +209,7 @@ const handleSelectionOnStickyPane = (store: ReactGridStore, cell: Cell, rowIndex
 
   // return tryExpandingTowardsCell(store, cell, rowIndex, colIndex);
   return store;
-}
+};
 
 export const CellSelectionBehavior: BehaviorConstructor = (setCurrentBehavior) => {
   const behavior: Behavior = {
