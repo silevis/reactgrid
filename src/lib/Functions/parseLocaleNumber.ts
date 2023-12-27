5140ef9f9d7b93a36a3757d1ececae9ada502b25
Michota <dawidmichota@gmail.com>
2023-12-27 09:07:52 +0100

    fix: re-write parseLocaleNumber
    - create function that will get decimal and thousand separators
    - replace these separators in strings passed as an stringNumber argument

--- src/lib/Functions/parseLocaleNumber.ts
+++ src/lib/Functions/parseLocaleNumber.ts
@@ -1,48 +1,23 @@
 import { getNavigatorLanguage } from "./getNavigatorLanguage";
 
-/**
- * Parses a locale-formatted number string into a JavaScript number.
- *
- * This function uses the Intl.NumberFormat API to handle locale-specific number formatting.
- * It replaces thousands separators and adjusts decimal separators based on the provided locale.
- *
- * @param {string} stringNumber - The locale-formatted number string to parse.
- * @param {string} [locale=getNavigatorLanguage()] - The locale to use for formatting. Defaults to the user's browser locale.
- * @returns {number} The parsed number, or NaN if parsing fails.
- *
- * @example
- * // Parse a number with the default locale
- * const result = parseLocaleNumber("1,234.56");
- * console.log(result); // Output: 1234.56
- *
- * @example
- * // Parse a number with a specific locale
- * const result = parseLocaleNumber("1.234,56", "de-DE");
- * console.log(result); // Output: 1234.56
- */
 
+function getLocaleSeparators(locale: string) {
+  const testNumber = 123456.789;
+  const localeFormattedNumber = Intl.NumberFormat(locale).format(testNumber);
 
-export function parseLocaleNumber(stringNumber: string, locale = getNavigatorLanguage()): number {
-  try {
-    // Use Intl.NumberFormat to get locale-specific separators
-    const numberFormat = new Intl.NumberFormat(locale);
-    const formatSample = numberFormat.formatToParts(12345.6);
+  // Get the thousands separator of the locale
+  const thousandsSeparator = localeFormattedNumber.split('123')[1][0]
 
-    // Check if the decimal separator is a comma
-    const decimalSeparatorIsComma = formatSample.some((part) => part.type === "decimal" && part.value === ",");
-
-    // Replace thousands separator and adjust decimal separator
-    const sanitizedNumberString = stringNumber
-      .replace(decimalSeparatorIsComma ? /[^\d,]/g : /[^\d.]/g, "")
-      .replace(decimalSeparatorIsComma ? "," : ".", ".");
-
-    // Parse the sanitized string to a number
-    const parsedNumber = Number.parseFloat(sanitizedNumberString);
-
-    return parsedNumber;
-  } catch (error) {
-    console.error(`Error parsing number: ${stringNumber}`, error);
-    return NaN;
-  }
+  // Get the decimal separator of the locale
+  const decimalSeparator = localeFormattedNumber.split('123')[1][4]
+  return { thousandsSeparator, decimalSeparator }
 }
 
+export function parseLocaleNumber(stringNumber: string, locale = getNavigatorLanguage()): number {
+  const { thousandsSeparator, decimalSeparator } = getLocaleSeparators(locale)
+  const normalizedStringNumber = stringNumber.replace(/\u00A0/g, ' ') // Replace non-breaking space with normal space
+  const numberString = normalizedStringNumber
+    .replace(new RegExp(`[${thousandsSeparator}\\s]`, 'g'), '') // Replace thousands separator and white-space
+    .replace(new RegExp(`\\${decimalSeparator}`, 'g'), '.') // Replace decimal separator
+  return Number(numberString) 
+}
