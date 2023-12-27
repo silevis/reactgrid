fee1f18a84c79684120112b06c692d7d04f9cdef
Michota <dawidmichota@gmail.com>
2023-12-27 09:30:04 +0100

    chore: temporary remove test before push to develop
    fix: re-write parseLocaleNumber
    
    - create helper-function that will get decimal and thousand separators
    - replace these separators in strings passed as an stringNumber argument
    - replace all characters (like "USD", or "$", etc.) before and after numbers

--- src/lib/Functions/parseLocaleNumber.ts
+++ src/lib/Functions/parseLocaleNumber.ts
@@ -13,11 +13,26 @@ function getLocaleSeparators(locale: string) {
   return { thousandsSeparator, decimalSeparator }
 }
 
+function getLocaleSeparators(locale: string) {
+  const testNumber = 123456.789;
+  const localeFormattedNumber = Intl.NumberFormat(locale).format(testNumber);
+
+  // Get the thousands separator of the locale
+  const thousandsSeparator = localeFormattedNumber.split('123')[1][0]
+
+  // Get the decimal separator of the locale
+  const decimalSeparator = localeFormattedNumber.split('123')[1][4]
+  return { thousandsSeparator, decimalSeparator }
+}
+
 export function parseLocaleNumber(stringNumber: string, locale = getNavigatorLanguage()): number {
   const { thousandsSeparator, decimalSeparator } = getLocaleSeparators(locale)
   const normalizedStringNumber = stringNumber.replace(/\u00A0/g, ' ') // Replace non-breaking space with normal space
   const numberString = normalizedStringNumber
     .replace(new RegExp(`[${thousandsSeparator}\\s]`, 'g'), '') // Replace thousands separator and white-space
     .replace(new RegExp(`\\${decimalSeparator}`, 'g'), '.') // Replace decimal separator
-  return Number(numberString) 
+  
+  const trimmedNumberString = numberString.replace(/^[^\d]+|[^\d]+$/g, ''); // Remove characters before first and after last number
+  
+  return Number(trimmedNumberString) 
 }
