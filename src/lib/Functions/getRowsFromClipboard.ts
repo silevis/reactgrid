552b2266ce084fafdf843a077e65f010f9fe5be2
Daniel Fidor <daniel.fidor@silevis.com>
2023-12-28 17:53:50 +0100

    getRowsFromClipboard: readability and security improvements, added types Added missing types, error handles, moved parts of the logic to separate functions, renamed functions and variables to better indicate their purpose

--- src/lib/Functions/getRowsFromClipboard.ts
+++ src/lib/Functions/getRowsFromClipboard.ts
@@ -1,49 +1,79 @@
 import { Cell, Compatible } from "../Model/PublicModel";
 import { parseLocaleNumber } from "./parseLocaleNumber";
 
-async function getPlainTextFromClipboard() {
-  const text = await navigator.clipboard.readText();
-  const dataForCells = text
-    .split("\n")
-    .map((line) => line.split("\t").map((t) => ({ type: "text", text: t, value: parseLocaleNumber(t) })));
-  return dataForCells;
+async function getCellsFromClipboardPlainText(): Promise<Compatible<Cell>[][]> {
+  const text = await navigator.clipboard.readText().catch(() => {
+    throw new Error("Failed to read textual data from clipboard!");
+  });
+
+  return text.split("\n").map((line) =>
+    line.split("\t").map((textValue) => ({
+      type: "text",
+      text: textValue,
+      value: parseLocaleNumber(textValue),
+    }))
+  );
+}
+
+async function getDocumentFromHTMLClipboardItem(HTMLItem: ClipboardItem): Promise<Document> {
+  const HTMLBlob = await HTMLItem.getType("text/html").catch(() => {
+    throw new Error("Failed to get HTML Blob data from clipboard!");
+  });
+  const HTMLString = await HTMLBlob.text().catch(() => {
+    throw new Error("Failed to parse HTML Blob to text!");
+  });
+
+  try {
+    const document = new DOMParser().parseFromString(HTMLString, "text/html");
+
+    return document;
+  } catch (e) {
+    throw new Error("Failed to parse HTML string to DOM!");
+  }
+}
+
+async function getCellsFromDocumentBody(documentBody: HTMLElement): Promise<Compatible<Cell>[][]> {
+  const pastedRows: Compatible<Cell>[][] = [];
+
+  if (!documentBody.firstElementChild?.firstElementChild) {
+    return await getCellsFromClipboardPlainText();
+  }
+
+  const tableRows = documentBody.firstElementChild.firstElementChild.children;
+
+  for (let rowIdx = 0; rowIdx < tableRows.length; rowIdx++) {
+    const row: Compatible<Cell>[] = [];
+
+    for (let colIdx = 0; colIdx < tableRows[rowIdx].children.length; colIdx++) {
+      const rawData = tableRows[rowIdx].children[colIdx].getAttribute("data-reactgrid");
+      const data = rawData && JSON.parse(rawData);
+      const text = tableRows[rowIdx].children[colIdx].textContent ?? "";
+
+      row.push(data ? data : { type: "text", text, value: parseLocaleNumber(text) });
+    }
+
+    pastedRows.push(row);
+  }
+
+  return pastedRows;
 }
 
 export default async (): Promise<Compatible<Cell>[][]> => {
-  // Pasted data from clipboard
-  let pastedRows: Compatible<Cell>[][] = [];
-
-  // A1. Check if clipboard contains plain/HTML type of data
   const clipboardItems = await navigator.clipboard.read();
-  const HTMLItem = clipboardItems.find((item) => {
-    if (item.types.includes("text/html")) {
-      return true;
-    }
-  });
-  // B. If it's plain data, then get it, and assign it to pastedRows
-  if (!HTMLItem) {
-    pastedRows = await getPlainTextFromClipboard();
+
+  // TODO: Support multiple clipboard items
+  // Find the first clipboard item that has HTML data
+  const HTMLItem = clipboardItems.find((item) => item.types.includes("text/html"));
+
+  // If the clipboard item with HTML data is found, try to parse it...
+  const document = HTMLItem ? await getDocumentFromHTMLClipboardItem(HTMLItem) : null;
+  const hasReactGridContent = document?.body.firstElementChild?.getAttribute("data-reactgrid") === "reactgrid-content";
+
+  // If the parsed document has ReactGrid content, get the cells from the document body
+  if (hasReactGridContent) {
+    return getCellsFromDocumentBody(document.body);
   }
-  // A2. If it's HTML data, then parse it, and assign it to pastedRows
-  const HTMLString = (await HTMLItem?.getType("text/html")?.then((blob) => blob.text())) as string;
-  const document = new DOMParser().parseFromString(HTMLString, "text/html");
-  const hasReactGridAttribute = document.body.firstElementChild?.getAttribute("data-reactgrid") === "reactgrid-content";
-  if (hasReactGridAttribute && document.body.firstElementChild?.firstElementChild) {
-    const tableRows = document.body.firstElementChild.firstElementChild.children;
-    for (let ri = 0; ri < tableRows.length; ri++) {
-      const row: Compatible<Cell>[] = [];
-      for (let ci = 0; ci < tableRows[ri].children.length; ci++) {
-        const rawData = tableRows[ri].children[ci].getAttribute("data-reactgrid");
-        const data = rawData && JSON.parse(rawData);
-        const text = tableRows[ri].children[ci].innerHTML;
-        row.push(data ? data : { type: "text", text, value: parseLocaleNumber(text) });
-      }
-      pastedRows.push(row);
-    }
-  }
-  // A3. If it's HTML data, but not from ReactGrid, then get plain data, and assign it to pastedRows
-  else {
-    pastedRows = await getPlainTextFromClipboard();
-  }
-  return pastedRows;
+
+  // ...otherwise, get the cells from the clipboard's textual data
+  return await getCellsFromClipboardPlainText();
 };
