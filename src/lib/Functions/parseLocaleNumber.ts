58b8f9e2064077a9d91d42933e7222843fbd24eb
Daniel Fidor <daniel.fidor@silevis.com>
2023-12-28 17:57:57 +0100

    parseLocaleNumber: added disclaimer; formatting

--- src/lib/Functions/parseLocaleNumber.ts
+++ src/lib/Functions/parseLocaleNumber.ts
@@ -1,28 +1,27 @@
 import { getNavigatorLanguage } from "./getNavigatorLanguage";
 
-
+// ! Won't work with locales using characters different than Arabic numerals (e.g. *Eastern* Arabic numerals: ١٢٣٬٤٥٦٫٧٨٩)
+// TODO: If possible add support for locales using characters different than Arabic numerals
 function getLocaleSeparators(locale: string) {
   const testNumber = 123456.789;
   const localeFormattedNumber = Intl.NumberFormat(locale).format(testNumber);
 
   // Get the thousands separator of the locale
-  const thousandsSeparator = localeFormattedNumber.split('123')[1][0]
+  const thousandsSeparator = localeFormattedNumber.split("123")[1][0];
 
   // Get the decimal separator of the locale
-  const decimalSeparator = localeFormattedNumber.split('123')[1][4]
-  return { thousandsSeparator, decimalSeparator }
+  const decimalSeparator = localeFormattedNumber.split("123")[1][4];
+  return { thousandsSeparator, decimalSeparator };
 }
 
-
-
 export function parseLocaleNumber(stringNumber: string, locale = getNavigatorLanguage()): number {
-  const { thousandsSeparator, decimalSeparator } = getLocaleSeparators(locale)
-  const normalizedStringNumber = stringNumber.replace(/\u00A0/g, ' ') // Replace non-breaking space with normal space
+  const { thousandsSeparator, decimalSeparator } = getLocaleSeparators(locale);
+  const normalizedStringNumber = stringNumber.replace(/\u00A0/g, " "); // Replace non-breaking space with normal space
   const numberString = normalizedStringNumber
-    .replace(new RegExp(`[${thousandsSeparator}\\s]`, 'g'), '') // Replace thousands separator and white-space
-    .replace(new RegExp(`\\${decimalSeparator}`, 'g'), '.') // Replace decimal separator
-  
-  const trimmedNumberString = numberString.replace(/^[^\d]+|[^\d]+$/g, ''); // Remove characters before first and after last number
-  
-  return Number(trimmedNumberString) 
+    .replace(new RegExp(`[${thousandsSeparator}\\s]`, "g"), "") // Replace thousands separator and white-space
+    .replace(new RegExp(`\\${decimalSeparator}`, "g"), "."); // Replace decimal separator
+
+  const trimmedNumberString = numberString.replace(/^[^\d]+|[^\d]+$/g, ""); // Remove characters before first and after last number
+
+  return Number(trimmedNumberString);
 }
