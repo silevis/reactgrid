9cc7bcb1cf5e26f21fd51cff0e57d4cfc61f538c
Michota <dawidmichota@gmail.com>
2023-12-29 11:42:13 +0100

    style: remove unnecessary code

--- src/lib/Functions/getNavigatorLanguage.ts
+++ src/lib/Functions/getNavigatorLanguage.ts
@@ -1,8 +1,1 @@
-export const getNavigatorLanguage = (): string => navigator.languages && navigator.languages.length
-  ? navigator.languages[0]
-  :
-  // .userLanguage is a IE feature...
-  // navigator.userLanguage || 
-  navigator.language ||
-  // navigator.browserLanguage ||
-  "en-US";
+export const getNavigatorLanguage = (): string => navigator.language || "en-US";
