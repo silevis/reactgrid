9d74c31c88523c4cb2b1d7457b8300bb2fea39e3
Daniel Fidor <daniel.fidor@silevis.com>
2024-01-19 14:44:57 +0100

    added callbacks and native capture events to RGProps Added callbacks and native event handlers to ReactGrid props as well as properties related to functionalities handled by the aforementioned callbacks

--- src/types/PublicModel.ts
+++ src/types/PublicModel.ts
@@ -1,15 +1,17 @@
-import React, { CSSProperties } from "react";
+import React from "react";
 import { BehaviorConstructor } from "./Behavior";
+import { IndexedLocation } from "./InternalModel";
+import { NumericalRange } from "./CellMatrix";
 
 export type Row<Id = string> = {
   id: Id;
   height: string | number;
-}
+};
 
 export type Column<Id = string> = {
   id: Id;
   width: string | number;
-}
+};
 
 export type Cell<RowIdType extends string = string, ColIdType extends string = string> = {
   /** User defined row ID, must exist in the `rows` array! */
@@ -22,7 +24,7 @@ export type Cell<RowIdType extends string = string, ColIdType extends string = s
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Template: React.ComponentType<any>;
   /** Props passed to the cell's template. Types and structure is inherited from Template prop, but instead of JSX properties it's an object. */
-  props?: React.ComponentPropsWithRef<Cell['Template']>;
+  props?: React.ComponentPropsWithRef<Cell["Template"]>;
 
   /** Represents how many rows should the cell occupy. */
   rowSpan?: number;
@@ -32,12 +34,12 @@ export type Cell<RowIdType extends string = string, ColIdType extends string = s
   isFocusable?: boolean;
   /** Marks a cell as selectable or not */
   isSelectable?: boolean;
-}
+};
 
 export type SpanMember = {
   originRowId: string;
   originColId: string;
-}
+};
 
 export type CellContextType = {
   /** User defined row ID. */
@@ -54,7 +56,7 @@ export type CellContextType = {
   rowSpan?: number;
   /** Represents how many columns should the cell occupy. */
   colSpan?: number;
-  
+
   /** Internal: provides cell container's style  */
   containerStyle: React.CSSProperties;
 
@@ -65,17 +67,58 @@ export type CellContextType = {
 
   isInEditMode: boolean;
   isFocused: boolean;
-}
+};
 
-export type CellMap<RowIdType extends string = string, ColIdType extends string = string> = Map<`${RowIdType} ${ColIdType}`, Cell<RowIdType, ColIdType> | SpanMember>;
+export type CellMap<RowIdType extends string = string, ColIdType extends string = string> = Map<
+  `${RowIdType} ${ColIdType}`,
+  Cell<RowIdType, ColIdType> | SpanMember
+>;
 
-export interface ReactGridProps {
+type EventHandlers = Omit<React.DOMAttributes<HTMLDivElement>, "dangerouslySetInnerHTML" | "children">;
+
+// Based on: https://stackoverflow.com/a/74852313
+// Returns a type with keys that end with the given string
+type PickEndsWith<Type extends object, SearchString extends string> = {
+  [Key in keyof Type as Key extends `${infer R}${SearchString}` ? Key : never]: Type[Key];
+};
+
+export type CaptureEventHandlers = PickEndsWith<EventHandlers, "Capture">;
+
+export type ReactGridCallbacks = {
+  onFocusLocationChanging?: (location: IndexedLocation) => boolean;
+  onFocusLocationChanged?: (location: IndexedLocation) => void;
+
+  onSelectionChanging?: (area: NumericalRange) => boolean;
+  onSelectionChanged?: (area: NumericalRange) => void;
+
+  onFillHandle?: (event: React.PointerEvent<HTMLDivElement>, area: NumericalRange) => void;
+
+  onRowSelection?: (event: React.PointerEvent<HTMLDivElement>, rowId: string) => void;
+  onRowResize?: (
+    event: React.PointerEvent<HTMLDivElement>,
+    rowId: string,
+    oldHeightInPx: number,
+    newHeightInPx: number
+  ) => void;
+  onRowReorder?: (event: React.PointerEvent<HTMLDivElement>, rowIds: string[]) => void;
+
+  onColumnSelection?: (event: React.PointerEvent<HTMLDivElement>, colId: string) => void;
+  onColumnResize?: (
+    event: React.PointerEvent<HTMLDivElement>,
+    colId: string,
+    oldWidthInPx: number,
+    newWidthInPx: number
+  ) => void;
+  onColumnReorder?: (event: React.PointerEvent<HTMLDivElement>, colIds: string[]) => void;
+};
+
+export interface ReactGridProps extends CaptureEventHandlers, ReactGridCallbacks {
   id: string;
 
   style?: React.CSSProperties;
 
-  columns: Column[];
   rows: Row[];
+  columns: Column[];
 
   cells: CellMap;
 
@@ -85,13 +128,19 @@ export interface ReactGridProps {
   stickyLeftColumns?: number;
 
   // enableVirtualization?: boolean;
+  enableFillHandle?: boolean;
+  fillHandleDirection?: "both" | "horizontal" | "vertical";
 
-  behaviors?: Record<string, BehaviorConstructor>
+  enableRowSelection?: boolean;
+  enableRowResize?: boolean;
+  enableRowReorder?: boolean;
 
-  focusLocation?: [number, number]
-  initialFocusLocation?: [number, number]
+  enableColumnSelection?: boolean;
+  enableColumnResize?: boolean;
+  enableColumnReorder?: boolean;
 
-  onFocusLocationChanging?: ({ location }: { location: [number, number] }) => boolean;
-  onFocusLocationChanged?: ({ location }: { location: [number, number] }) => void;
+  behaviors?: Record<string, BehaviorConstructor>;
+
+  focusLocation?: [number, number];
+  initialFocusLocation?: [number, number];
 }
-
