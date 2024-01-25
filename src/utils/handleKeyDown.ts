7912e53831d982b02217d2eb6355f29a8f272e34
Dawid Michota <109215822+Michota@users.noreply.github.com>
2024-01-25 14:42:45 +0100

    Manage selected area with keyboard (#312)
    * WIP:  tryExpandingTowardsDirection
    
    * check if focused cell is in range after modifying selected area
    
    * feat: add shortcut CTRL + SPACE BAR
    
    - CTRL + SPACE BAR: select all rows in currently selected columns
    
    * feat: add shortcut SHIFT + SPACE BAR
    
    - SHIFT + SPACE BAR: select all columns in currently selected rows
    
    * fix: Don't allow to go beyond the grid
    
    - Do not allow user to go beyond the grid using with-keyboard selecting
    
    * feat: Add selection shortcuts for arrow keys.
    
    * refactor: improve readability and maintainability
    
    * feat: add jump-to-last-cell feature
    
    - by clicking CTRL + Arrow, move to the last cell in selected direction.
    
    * docs: add code-comments
    
    * feat: add CTRL + Home/End
    
    - make it behave like Home/End, but in y-axis.
    
    * refactor: make function name more self-descriptive
    
    * fix: remove selection on focus-jump
    
    * moved tab to one case, comment improvements
    
    * comment improvements, changed to JSDoc
    
    * renamed resizeTowardsDirection to resizeSelectionInDirection
    
    ---------
    
    Co-authored-by: Daniel Fidor <daniel.fidor@silevis.com>

--- src/utils/handleKeyDown.ts
+++ src/utils/handleKeyDown.ts
@@ -1,16 +1,29 @@
 import { NumericalRange } from "../types/CellMatrix";
-import { EMPTY_AREA, areAreasEqual } from "./cellUtils";
-import { moveFocusDown, moveFocusInsideSelectedRange, moveFocusLeft, moveFocusRight, moveFocusUp } from "./focus";
+import { EMPTY_AREA, areAreasEqual, findMinimalSelectedArea, getCellArea } from "./cellUtils";
+import {
+  moveFocusDown,
+  moveFocusInsideSelectedRange,
+  moveFocusLeft,
+  moveFocusRight,
+  moveFocusUp,
+} from "./focus";
 import { ReactGridStore } from "./reactGridStore";
+import { resizeSelectionInDirection } from "./resizeSelectionInDirection";
+
+// ? Problem: The more complicated is the key-combination (the more keys are included), the higher-in-code it has to be.
+// * By that I mean it has to be executed earlier.
 
 export const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>, store: ReactGridStore): ReactGridStore => {
-  if (event.altKey || store.currentlyEditedCell.rowIndex !== -1 || store.currentlyEditedCell.colIndex !== -1) return store;
+  if (event.altKey || store.currentlyEditedCell.rowIndex !== -1 || store.currentlyEditedCell.colIndex !== -1)
+    return store;
 
+  // Check if there is focusedCell
   let focusedCell = store.getFocusedCell();
   if (!focusedCell) {
     const firstCell = store.getCellByIndexes(0, 0);
     if (!firstCell) return store;
 
+    // If there is no focused cell, set it to the first cell in the grid.
     focusedCell = {
       rowIndex: 0,
       colIndex: 0,
@@ -18,8 +31,111 @@ export const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>, store:
     };
   }
 
+  const isAnyAreaSelected = !areAreasEqual(store.selectedArea, EMPTY_AREA);
+
+  // * SHIFT + CTRL/COMMAND (⌘) + <Key>
+  if (event.shiftKey && (event.ctrlKey || event.metaKey)) {
+    switch (event.key) {
+      // Select all rows according to columns in currently selected area OR focused cell area.
+      case "ArrowUp": {
+        event.preventDefault();
+        // Get currently selected area
+        let area: NumericalRange = { ...store.selectedArea };
+
+        // If there is no selected area, get focused cell area
+        const isAnyAreaSelected = !areAreasEqual(area, EMPTY_AREA);
+        if (!isAnyAreaSelected) {
+          area = getCellArea(store, focusedCell);
+        }
+
+        // Get the area occupied by cells in the selected columns.
+        // Expand the obtained area by the area of cells that are spanned-cells, in selected direction (up).
+        const areaWithSpannedCells = findMinimalSelectedArea(store, {
+          ...area,
+          startRowIdx: Number(store.rows[0].id),
+        });
+
+        // Select all cells in obtained area, including spanned cells.
+        return { ...store, selectedArea: { ...areaWithSpannedCells } };
+      }
+
+      // Select all rows according to columns in currently selected area OR focused cell area.
+      case "ArrowDown": {
+        event.preventDefault();
+        // Get currently selected area
+        let area: NumericalRange = { ...store.selectedArea };
+
+        // If there is no selected area, get focused cell area
+        const isAnyAreaSelected = !areAreasEqual(area, EMPTY_AREA);
+        if (!isAnyAreaSelected) {
+          area = getCellArea(store, focusedCell);
+        }
+
+        // Get the area occupied by cells in the selected columns.
+        // Expand the obtained area by the area of cells that are spanned-cells, in selected direction (down).
+        const areaWithSpannedCells = findMinimalSelectedArea(store, {
+          ...area,
+          endRowIdx: store.rows.length,
+        });
+
+        // Select all cells in obtained area, including spanned cells.
+        return { ...store, selectedArea: { ...areaWithSpannedCells } };
+      }
+
+      // Select all columns according to rows in currently selected area OR focused cell area.
+      case "ArrowLeft": {
+        event.preventDefault();
+        // Get currently selected area
+        let area: NumericalRange = { ...store.selectedArea };
+
+        // If there is no selected area, get focused cell area
+        const isAnyAreaSelected = !areAreasEqual(area, EMPTY_AREA);
+        if (!isAnyAreaSelected) {
+          area = getCellArea(store, focusedCell);
+        }
+
+        // Get the area occupied by cells in the selected rows.
+        // Expand the obtained area by the area of cells that are spanned-cells, in selected direction (to left).
+        const areaWithSpannedCells = findMinimalSelectedArea(store, {
+          ...area,
+          startColIdx: Number(store.columns[0].id),
+        });
+
+        // Select all cells in obtained area, including spanned cells.
+        return { ...store, selectedArea: { ...areaWithSpannedCells } };
+      }
+
+      // Select all columns according to rows in currently selected area OR focused cell area.
+      case "ArrowRight": {
+        event.preventDefault();
+        // Get currently selected area
+        let area: NumericalRange = { ...store.selectedArea };
+
+        // If there is no selected area, get focused cell area
+        const isAnyAreaSelected = !areAreasEqual(area, EMPTY_AREA);
+        if (!isAnyAreaSelected) {
+          area = getCellArea(store, focusedCell);
+        }
+
+        // Get the area occupied by cells in the selected rows.
+        // Expand the obtained area by the area of cells that are spanned-cells, in selected direction (to right).
+        const areaWithSpannedCells = findMinimalSelectedArea(store, {
+          ...area,
+          endColIdx: store.columns.length,
+        });
+
+        // Select all cells in obtained area, including spanned cells.
+        return { ...store, selectedArea: { ...areaWithSpannedCells } };
+      }
+      default:
+        return store;
+    }
+  }
+
+  // * CTRL/COMMAND (⌘) + <Key>
   if (event.ctrlKey || event.metaKey) {
     switch (event.key) {
+      // Select all cells in the grid.
       case "a": {
         event.preventDefault();
 
@@ -28,8 +144,8 @@ export const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>, store:
           endRowIdx: store.rows.length,
           startColIdx: 0,
           endColIdx: store.columns.length,
-        }
-
+        };
+        // If the whole grid is already selected, deselect it.
         if (areAreasEqual(store.selectedArea, wholeGridArea)) {
           return {
             ...store,
@@ -47,20 +163,104 @@ export const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>, store:
           selectedArea: wholeGridArea,
         };
       }
+
+      // Jump to the cell that is in the first row, but in the same column as the focused cell.
+      case "ArrowUp":
+      case "Home": {
+        event.preventDefault();
+        if (!focusedCell) return store;
+        return { ...store, focusedLocation: { ...store.focusedLocation, rowIndex: 0 }, selectedArea: EMPTY_AREA };
+      }
+      // Jump to the cell that is in the last row, but in the same column as the focused cell.
+      case "ArrowDown":
+      case "End": {
+        event.preventDefault();
+        if (!focusedCell) return store;
+        return { ...store, focusedLocation: { ...store.focusedLocation, rowIndex: store.rows.length - 1 }, selectedArea: EMPTY_AREA };
+      }
+      // Jump to the cell that is in the first column, but in the same row as the focused cell.
+      case "ArrowLeft": {
+        event.preventDefault();
+        if (!focusedCell) return store;
+        return { ...store, focusedLocation: { ...store.focusedLocation, colIndex: 0 }, selectedArea: EMPTY_AREA };
+      }
+      // Jump to the cell that is in the last column, but in the same row as the focused cell.
+      case "ArrowRight": {
+        event.preventDefault();
+        if (!focusedCell) return store;
+        return { ...store, focusedLocation: { ...store.focusedLocation, colIndex: store.columns.length - 1 }, selectedArea: EMPTY_AREA };
+      }
+
+      // Select all rows according to columns in currently selected area OR focused cell area.
+      case " ": {
+        event.preventDefault();
+        // Get currently selected area
+        let area: NumericalRange = { ...store.selectedArea };
+
+        // If there is no selected area, get focused cell area
+        const isAnyAreaSelected = !areAreasEqual(area, EMPTY_AREA);
+        if (!isAnyAreaSelected) {
+          area = getCellArea(store, focusedCell);
+        }
+
+        // Get the area occupied by cells in the selected columns.
+        // Expand the obtained area by the area of cells that are spanned-cells.
+        const areaWithSpannedCells = findMinimalSelectedArea(store, {
+          ...area,
+          startRowIdx: Number(store.rows[0].id),
+          endRowIdx: store.rows.length,
+        });
+
+        // Select all cells in obtained area, including spanned cells.
+        return { ...store, selectedArea: { ...areaWithSpannedCells } };
+      }
+
+      // If nothing has changed, return the store as it is.
       default:
         return store;
     }
   }
 
-  const isAnyAreaSelected = !areAreasEqual(store.selectedArea, EMPTY_AREA);
-
-  if (isAnyAreaSelected) {
+  // * SHIFT + <Key>
+  if (event.shiftKey) {
     switch (event.key) {
-      case "Tab": {
+      // Manage selection by expanding/shrinking it towards the direction of the arrow key.
+      case "ArrowUp":
         event.preventDefault();
+        return resizeSelectionInDirection(store, focusedCell, "Up");
+      case "ArrowDown":
+        event.preventDefault();
+        return resizeSelectionInDirection(store, focusedCell, "Down");
+      case "ArrowLeft":
+        event.preventDefault();
+        return resizeSelectionInDirection(store, focusedCell, "Left");
+      case "ArrowRight":
+        event.preventDefault();
+        return resizeSelectionInDirection(store, focusedCell, "Right");
 
-        if (event.shiftKey) return moveFocusInsideSelectedRange(store, focusedCell, "left");
-        else return moveFocusInsideSelectedRange(store, focusedCell, "right");
+      // Select all columns according to rows in currently selected area OR focused cell area.
+      // SPACE BAR
+      case " ": {
+        event.preventDefault();
+        // Get currently selected area
+        let area: NumericalRange = { ...store.selectedArea };
+
+        // If there is no selected area, get focused cell area
+        const isAnyAreaSelected = !areAreasEqual(area, EMPTY_AREA);
+        if (!isAnyAreaSelected) {
+          area = getCellArea(store, focusedCell);
+        }
+
+        // Get the area occupied by cells in the selected rows.
+        // Expand the obtained area by the area of cells that are spanned-cells.
+        const areaWithSpannedCells = findMinimalSelectedArea(store, {
+          ...area,
+          startColIdx: Number(store.columns[0].id),
+          endColIdx: store.columns.length,
+        });
+
+        // Select all cells in obtained area, including spanned cells.
+        return { ...store, selectedArea: { ...areaWithSpannedCells } };
       }
       case "Enter": {
         event.preventDefault();
@@ -70,27 +270,47 @@ export const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>, store:
       }
     }
   }
-  
+
+  // * Other key-downs (non-combinations)
+
   switch (event.key) {
+    // Move focus to next cell.
     case "Tab": {
       event.preventDefault();
 
       if (event.shiftKey) {
+        // If shift is pressed, move focus to the left...
+        if (isAnyAreaSelected) {
+          // ...inside selected range if any range is selected, or...
+          return moveFocusInsideSelectedRange(store, focusedCell, "left");
+        }
+
+        // ...to the left if no range is selected.
         return moveFocusLeft(store, focusedCell);
       } else {
+        // If shift is not pressed, move focus to the right...
+        if (isAnyAreaSelected) {
+          // ...inside selected range if any range is selected, or...
+          return moveFocusInsideSelectedRange(store, focusedCell, "right");
+        }
+
+        // ...to the right if no range is selected.
         return moveFocusRight(store, focusedCell);
       }
     }
+
+    // ! May create conflict with other Edit-mode.
     case "Enter": {
       event.preventDefault();
 
       if (event.shiftKey) {
-        return moveFocusUp(store, focusedCell);
+        return moveFocusUp(store, focusedCell); // If shift is pressed, move focus up (row up).
       } else {
-        return moveFocusDown(store, focusedCell);
+        return moveFocusDown(store, focusedCell); // Otherwise, move focus down (row down).
       }
-    }   
+    }
 
+    // Move focus to adjacent cell, according to the direction that arrow key points to.
     case "ArrowUp":
       event.preventDefault();
       return moveFocusUp(store, focusedCell);
@@ -104,7 +324,8 @@ export const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>, store:
       event.preventDefault();
       return moveFocusRight(store, focusedCell);
 
-    case "Home": 
+    // Move focus to the first/last cell in the row.
+    case "Home": {
       event.preventDefault();
       return {
         ...store,
@@ -112,17 +333,25 @@ export const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>, store:
           rowIndex: focusedCell.rowIndex,
           colIndex: 0,
         },
+        // If any area is selected, remove it.
+        selectedArea: EMPTY_AREA,
       };
-    case "End":
+    }
+    case "End": {
       event.preventDefault();
+
       return {
         ...store,
         focusedLocation: {
           rowIndex: focusedCell.rowIndex,
           colIndex: store.columns.length - 1,
         },
+        // If any area is selected, remove it.
+        selectedArea: EMPTY_AREA,
       };
-    
+    }
+
+    // TODO: Implement PageUp and PageDown
     case "PageUp":
       event.preventDefault();
       return store;
