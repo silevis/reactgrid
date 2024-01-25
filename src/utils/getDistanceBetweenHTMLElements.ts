f9bf9146ee3ff947833a49e8a629e6c5050e8116
Dawid Michota <109215822+Michota@users.noreply.github.com>
2024-01-25 11:55:00 +0100

    Update getDistanceBetweenHTMLElements.ts

--- src/utils/getDistanceBetweenHTMLElements.ts
+++ src/utils/getDistanceBetweenHTMLElements.ts
@@ -1,22 +1,19 @@
 export function getOffset(element: Element): { top: number; left: number; } | null {
-  if (!element.getClientRects().length) {
-    return null;
-  }
-
   const rect = element.getBoundingClientRect();
-  const win = element.ownerDocument.defaultView;
-  if (!win) return null;
+  
   return {
     top: rect.top + window.scrollY,
     left: rect.left + window.scrollY,
   };
 }
+
 export function getDistanceBetweenOffsets(firstEl: HTMLElement, secondEl: HTMLElement) {
   const firstOffset = getOffset(firstEl);
   if (!firstOffset) return null;
 
   const secondOffset = getOffset(secondEl);
   if (!secondOffset) return null;
+  
   return { left: secondOffset.left - firstOffset.left, top: secondOffset.top - firstOffset.top };
 }
 
