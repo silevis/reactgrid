4d480054a336f574c3eb6d4b56acc0bcbd3cb3fe
Michota <dawidmichota@gmail.com>
2024-02-06 09:15:48 +0100

    feat: create isTheSameCell util

---
+++ src/utils/isTheSameCell.ts
@@ -0,0 +1,25 @@
+import { Cell } from "../types/PublicModel";
+import { getCellIndexesFromContainerElement } from "./getCellIndexes";
+
+export function isTheSameCell(firstCell: Cell, secondCell: Cell): boolean;
+export function isTheSameCell(firstCell: HTMLElement, secondCell: HTMLElement): boolean;
+
+export function isTheSameCell(firstCell: Cell | HTMLElement, secondCell: Cell | HTMLElement): boolean | null {
+  const isBasedOnContainers = "rowId" in firstCell === false && "rowId" in secondCell === false;
+
+  if (isBasedOnContainers) {
+    const firstIndexes = getCellIndexesFromContainerElement(firstCell);
+    const secondIndexes = getCellIndexesFromContainerElement(firstCell);
+
+    return (
+      firstIndexes &&
+      secondIndexes &&
+      firstIndexes.rowIndex === secondIndexes.rowIndex &&
+      firstIndexes.colIndex === secondIndexes.colIndex
+    );
+  } else if ("rowId" in firstCell && "rowId" in secondCell) {
+    return firstCell.rowId === secondCell.rowId && firstCell.colId === secondCell.colId;
+  } else {
+    return null;
+  }
+}
