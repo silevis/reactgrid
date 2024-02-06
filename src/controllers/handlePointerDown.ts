789444ebe08f6f125872a03e1aa5ecafd161a546
Michota <dawidmichota@gmail.com>
2024-02-06 08:37:13 +0100

    fix: remove listener on move

--- src/controllers/handlePointerDown.ts
+++ src/controllers/handlePointerDown.ts
@@ -4,7 +4,7 @@ import { StoreApi } from "zustand";
 import { ReactGridStore } from "../utils/reactGridStore.ts";
 import { updateStoreWithApiAndEventHandler } from "../utils/updateStoreWithApiAndEventHandler.ts";
 
-export const handlePointerDown = (event: React.PointerEvent<HTMLDivElement>, storeApi: StoreApi<ReactGridStore>) => {
+export const handlePointerDown = (pointerDownEvent: React.PointerEvent<HTMLDivElement>, storeApi: StoreApi<ReactGridStore>) => {
   const withStoreApi = <TEvent extends React.SyntheticEvent<HTMLDivElement>>(
     event: TEvent,
     handler?: HandlerFn<TEvent>
@@ -12,25 +12,24 @@ export const handlePointerDown = (event: React.PointerEvent<HTMLDivElement>, sto
 
   const store = storeApi.getState();
 
-  const handlePointerMove = (event: PointerEvent) => {
+  const handlePointerMove = (pointerMoveEvent: PointerEvent) => {};
+
+  const handlePointerUp = (pointerUpEvent: PointerEvent) => {
+    // Remove listeners after pointerUp
     window.removeEventListener("pointermove", handlePointerMove);
     window.removeEventListener("pointerup", handlePointerUp);
   };
 
-  const handlePointerUp = (event: PointerEvent) => {
-    window.removeEventListener("pointermove", handlePointerMove);
-    window.removeEventListener("pointerup", handlePointerUp);
-  };
-
+  // Remove listeners after pointerDown
   window.addEventListener("pointermove", handlePointerMove);
   window.addEventListener("pointerup", handlePointerUp);
 
+  // With specified order behavior in mind, handlePointerDown on each behavior.
   const behaviors = Object.entries(store.behaviors);
-
   behaviors.forEach(([_behaviorId, behavior]) => {
-    withStoreApi(event, behavior.handlePointerDown);
+    withStoreApi(pointerDownEvent, behavior.handlePointerDown);
 
-    if (event.isPropagationStopped()) {
+    if (pointerDownEvent.isPropagationStopped()) {
       return;
     }
   });
