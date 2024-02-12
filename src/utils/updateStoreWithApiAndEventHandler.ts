e3358d199d771ec7df5ad7cd1ce41b12df674066
Pawel Marek <webloopbox@gmail.com>
2024-02-12 11:13:41 +0100

    fix the visibility of focused cells

--- src/utils/updateStoreWithApiAndEventHandler.ts
+++ src/utils/updateStoreWithApiAndEventHandler.ts
@@ -5,12 +5,12 @@ import { ReactGridStore } from "./reactGridStore";
 export const updateStoreWithApiAndEventHandler = <TEvent extends React.SyntheticEvent<HTMLDivElement>>(
   storeApi: StoreApi<ReactGridStore>,
   event: TEvent,
-  handler?: HandlerFn<TEvent>,
+  handler?: HandlerFn<TEvent>
 ) => {
   const store = storeApi.getState();
-  
+
   if (handler) {
     const newStore = handler(event, store);
     storeApi.setState(newStore);
   }
-}
+};
