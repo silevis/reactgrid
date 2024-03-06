7eb36a010057d9860adea85606ce5453bad50325
qiufeihong2018 <15058301288@163.com>
2024-03-06 14:34:34 +0800

    feat: Enhance user experience - Optimize the clipping effect: After clipping cells, the cells produce ant lines, showing the edited effect - change to copyRange #336

--- src/lib/Components/CutRanges.tsx
+++ src/lib/Components/CutRanges.tsx
@@ -1,17 +1,13 @@
 import * as React from "react";
 import { PaneContentChild, Range } from "../../core";
 import { PartialArea } from "./PartialArea";
-import { isRangeIntersects } from "../Functions/isRangeIntersectsWith";
 
 export const CutRanges: React.FC<PaneContentChild> = ({ state, calculatedRange }) => {
-  console.log("calculatedRange", calculatedRange);
-  console.log("state.selectedRanges", state.selectedRanges);
   return (
     <>
-      {state.selectedRanges.map((range: Range, i: number) => {
-        return (
-          calculatedRange &&
-          isRangeIntersects(calculatedRange, range) && (
+      {state.copyRange &&
+        [state.copyRange].map((range: Range, i: number) => {
+          return (
             <PartialArea
               key={i}
               pane={calculatedRange}
@@ -19,9 +15,8 @@ export const CutRanges: React.FC<PaneContentChild> = ({ state, calculatedRange }
               className="rg-partial-area-cut-range"
               style={{}}
             />
-          )
-        );
-      })}
+          );
+        })}
     </>
   );
 };
