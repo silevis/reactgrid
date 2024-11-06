d14c2e62dd628c6a49dd13a06f3955bd5f9f127e
Pawel <webloopbox@gmail.com>
2024-11-06 15:14:22 +0100

    update dev support page

--- website/app/support/page.tsx
+++ website/app/support/page.tsx
@@ -1,78 +1,97 @@
 "use client";
 
 import Link from "next/link";
-import { MdQuestionMark, MdPerson, MdOutlineBugReport } from "react-icons/md";
 import { Shape1, Shape2 } from "@/components/bg-shapes";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 export default function FeaturesPage() {
   return (
     <div className="z-1 flex-1">
       <>
         <div
-          className={`absolute z-0 w-[100%] top-[-10%] left-[-50%] hidden lg:block`}
+          className={`absolute z-0 w-[1300px] top-[-5%] left-[-35%] hidden xl:block`}
         >
           <Shape1 />
         </div>
         <div
-          className={`absolute z-0 w-[100%] top-[0%] right-[-35%] hidden lg:block`}
+          className={`absolute z-0 w-[1350px] top-[10%] right-[-20%] hidden xl:block`}
         >
           <Shape2 />
         </div>
       </>
 
-      <section className="relative h-full z-2">
-        <div className="grid grid-cols-main grid-rows-2 pt-12 md:pt-32 md:pb-12">
-          <h1 className="col-start-1 col-end-13 sm:col-start-2 sm:col-end-12 xl:col-start-3 xl:col-end-11 text-xl md:text-2xl font-bold text-center bg-gradient-to-t from-green-primary to-green-light bg-clip-text text-transparent px-4">
-            Need support?
-          </h1>
-          <p className="col-start-1 col-end-13 sm:col-start-2 sm:col-end-12 xl:col-start-3 xl:col-end-11 text-black-secondary text-center text-xs md:text-sm px-4 font-bold">
-            If you have any inquiries or need assistance, our team is here to
-            help.
+      <section className="relative h-full z-2 flex flex-col xl:flex-row p-4 gap-4 max-w-screen-xl pt-12 gap-x-8 md:gap-x-40 md:pt-32 mb-[128px] mx-auto">
+        <div className="xl:flex-1 flex flex-col md:items-center xl:items-start xl:mt-24">
+          <h2 className="text-md sm:text-center xl:text-left md:text-xl pt-4 font-bold text-green-primary">
+            Need help with ReactGrid?
+          </h2>
+          <p className="mb-16 sm:text-center xl:text-left max-w-screen-md">
+            If you have questions about integration, features, or have
+            encountered a problem – contact us! We’re here to assist you and
+            provide the support you need.
+          </p>
+          <h2 className="text-md md:text-xl sm:text-center xl:text-left font-bold text-green-primary">
+            Dedicated Support
+          </h2>
+          <p className="mb-16 sm:text-center xl:text-left max-w-screen-md">
+            If you need more customization of ReactGrid, advanced consulting or
+            targeted assistance, our hourly rate is <strong>$59</strong>. Reach
+            out to discuss how we can help streamline your project with expert
+            support.
           </p>
         </div>
-        <div className="grid grid-cols-main md:pb-[250px] pt-8 md:pt-0 max-w-screen-3xl mx-auto">
-          <div className="col-start-1 col-end-13 sm:col-start-2 sm:col-end-12 xl:col-start-3 xl:col-end-11 text-black-secondary flex gap-x-4 2xl:gap-x-16 mb-[128px] justify-center flex-col md:flex-row px-4">
-            <div className="flex-1 flex flex-col items-center justify-start bg-[#ffffff3d] border p-4">
-              <MdQuestionMark size={120} color="#107c41" />
-              <h3 className="text-center font-bold">Got questions?</h3>
-              <p className="mt-4 text-center">
-                Submit a question on our{" "}
-                <Link
-                  className="text-green-primary"
-                  href="https://github.com/silevis/reactgrid/discussions"
-                  target="_blank"
-                >
-                  GitHub Discussions
-                </Link>{" "}
-                page
-              </p>
-            </div>
-            <div className="flex-1 flex flex-col items-center justify-start pt-8 md:pt-0 bg-[#ffffff3d] border p-4 mt-8 md:mt-0">
-              <MdPerson size={120} color="#107c41" />
-              <h3 className="text-center font-bold">
-                Need talented developers?
-              </h3>
-              <p className="mt-4 text-center">
-                Get a cloud native software development team 100% dedicated to
-                your product.
-              </p>
-            </div>
-            <div className="flex-1 flex flex-col items-center justify-start pt-8 md:pt-0 bg-[#ffffff3d] border p-4 mt-8 md:mt-0">
-              <MdOutlineBugReport size={120} color="#107c41" />
-              <h3 className="text-center font-bold">Technical issues?</h3>
-              <p className="mt-4 text-center">
-                Please create an issue on our{" "}
-                <Link
-                  className="text-green-primary"
-                  href="https://github.com/silevis/reactgrid/issues"
-                  target="_blank"
-                >
-                  GitHub Issues
-                </Link>{" "}
-                page
-              </p>
-            </div>
-          </div>
+        <div className="xl:flex-1 flex flex-col md:mt-0">
+          <h2 className="font-bold text-black-primary sm:text-center text-md md:text-xl mb-8">
+            Let us hear from you!
+          </h2>
+          <form className="self-center flex flex-col gap-4 max-w-[600px] w-full bg-[white] bg-opacity-60 p-4 md:p-12 border border-[#1c1c1c4d]">
+            <label className="flex flex-col text-2xs">
+              Email address*
+              <Input
+                type="email"
+                className="border border-[#1c1c1c4d] p-2 text-xs rounded-none"
+                required
+              />
+            </label>
+            <label className="flex flex-col text-2xs">
+              Company name
+              <Input
+                type="text"
+                className="border border-[#1c1c1c4d] p-2 text-xs rounded-none"
+              />
+            </label>
+            <label className="flex flex-col text-2xs">
+              Your message*
+              <Textarea
+                minLength={15}
+                className="border border-[#1c1c1c4d] p-2 text-xs rounded-none"
+                rows={8}
+                required
+              />
+            </label>
+            <Button
+              type="submit"
+              className="bg-green-primary text-[white] text-xs p-2 rounded-none"
+            >
+              Send
+            </Button>
+            <hr className="my-2" />
+            <p className="text-2xs text-gray-600">
+              The controller of your personal data is Silevis Software Sp. z
+              o.o., with a registered office at Sienkiewicza Street 17/3, 25-007
+              Kielce. Your personal data will be processed in order to contact
+              you when you use our contact form. Please see more information{" "}
+              <Link
+                href="/privacy-policy"
+                className="text-green-primary underline"
+              >
+                here
+              </Link>
+              .
+            </p>
+          </form>
         </div>
       </section>
     </div>
