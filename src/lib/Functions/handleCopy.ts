8f0b4f1ac854af80c871b015eed80591b738c064
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2025-03-12 09:30:19 -0300

    use navigator clipboard if has support

--- src/lib/Functions/handleCopy.ts
+++ src/lib/Functions/handleCopy.ts
@@ -15,8 +15,16 @@ export function handleCopy(event: ClipboardEvent, state: State, removeValues = f
 }
 
 export function copyDataCommands(event: ClipboardEvent, state: State, div: HTMLDivElement): void {
+    const supportNavigatorClipboard = !!navigator?.clipboard?.write;
+
     if (isBrowserSafari()) {
         event.clipboardData.setData('text/html', div.innerHTML);
+    } else if (supportNavigatorClipboard) {
+        const clipboardItemData = {
+          "text/html": div.innerHtml,
+        }; 
+        const clipboardItem = new ClipboardItem(clipboardItemData);
+        await navigator.clipboard.write([clipboardItem]);
     } else {
         document.body.appendChild(div);
         div.focus();
